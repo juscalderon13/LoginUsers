5b676090375e81d5455aa311ef14895fda583448
JappsDev <juscalderon13@gmail.com>
2023-09-12 10:19:27 -0600

    Validación de Formulario
    manejo del submit en los formularios

--- Login/src/components/UserForm.jsx
+++ Login/src/components/UserForm.jsx
@@ -14,7 +14,7 @@ export const UserForm = () => {
     const {username, password, email} = UserForm;
 
     const onInputChange = ({target}) =>{
-        console.log(target.value )
+        //console.log(target.value )
         const {name, value} = target;
         setUserForm({
             ...UserForm,
@@ -23,8 +23,21 @@ export const UserForm = () => {
         })
 
     }
+
+    const onSubmit = (event) =>{
+        event.preventDefault();
+        if(!username || !password || !email){
+            alert("Debe completar los datos del formulario!");
+            return;
+        }
+        console.log(UserForm);
+
+        //guardar el userForm en el listado de usuarios
+        setUserForm(initialUserForm)
+    }
+
     return(
-        <form>
+        <form onSubmit={onSubmit}>
             <input className="form-control my-3 w-75" placeholder="Username" name="username" value={username} onChange={onInputChange}/>
             <input className="form-control my-3 w-75" placeholder="Password" type="password" name="password" value={password} onChange={onInputChange}/>
             <input className="form-control my-3 w-75" placeholder="Email" name="email" value={email} onChange={onInputChange}/>
