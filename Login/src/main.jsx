b8a5f24c46f1976729a1bcafb0097c5ef617eb8d
JappsDev <juscalderon13@gmail.com>
2023-09-20 22:21:32 -0600

    Revert "Merge remote-tracking branch 'origin/main' into main"
    This reverts commit d0e1e323c93e406509150e7fc02c329ca0a15f62.

--- Login/src/main.jsx
+++ Login/src/main.jsx
@@ -1,19 +1,10 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
-<<<<<<< HEAD
 import {AppUsers} from './AppUsers'
 import './styles.css'
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
     <AppUsers />
-=======
-import App from './App.jsx'
-import './index.css'
-
-ReactDOM.createRoot(document.getElementById('root')).render(
-  <React.StrictMode>
-    <App />
->>>>>>> main
   </React.StrictMode>,
 )
